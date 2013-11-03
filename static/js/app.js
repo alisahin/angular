4bd9f85688d9654da6e7a3392f4872c0846c0b64
Ali Sahin <ali@digitalnatives.nl>
2013-11-03 16:58:26 +0100

    added css3 loader en post forms

--- static/js/app.js
+++ static/js/app.js
@@ -45,12 +45,12 @@ APP.factory('services', function ($http) {
 			$http.get('https://api.leaguevine.com/v1/games/?tournament_id=19389&pool_id=19219&fields=%5Bpool%2C%20id%2C%20team_1%2C%20team_1_score%2C%20team_2%2C%20team_2_score%5D').success(callback);
 		},
 		getById: function (id, callback) {
-			$http.get('https://api.leaguevine.com/v1/games/?tournament_id=19389&pool_id=19219&fields=%5Bpool%2C%20id%2C%20team_1%2C%20team_1_score%2C%20team_2%2C%20team_2_score%5D').success(callback);
+			$http.get('https://api.leaguevine.com/v1/games/' + id + '/?{}').success(callback);
 		},
-		setGameScore: function (data, callback) {
+		setGameScore: function (game, callback) {
 			var accessToken = '82996312dc';
-			$http.post('https://api.leaguevine.com/v1/game_scores/?access_token=' + accessToken + '?callback?', data, {
-				headers: {'Authorization': 'bearer ' + accessToken}
+			$http.post('https://api.leaguevine.com/v1/game_scores/?access_token=' + accessToken + '?callback?', game, {
+				headers: {'Content-Type': 'application/x-www-form-urlencoded', 'Authorization': 'bearer ' + accessToken}
 			}).success(callback);
 		}	
 	}
@@ -82,14 +82,14 @@ APP.controller('gameCtrl', function ($scope, $http, services) {
 })
 
 APP.controller('setScoreCtrl', function ($scope, $http, $routeParams, services) {
-	console.log($routeParams.id);
-	services.getById($routeParams.id, function (data) {
-		$scope.data = data;
+	// console.log($routeParams.id);
+	services.getById($routeParams.id, function (game) {
+		$scope.game = game;
 	}) 
 
 	$scope.submit = function () {
-		console.log(this.data);
-		services.setGameScore(this.data, function () {
+		console.log(this.game);
+		services.setGameScore(this.game, function () {
 			// $scope.flash = "success";
 			console.log("Gelukt!");
 		})
